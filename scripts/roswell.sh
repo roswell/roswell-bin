5fa5733b1db2c6dc4fb15d3a98eca0a1a610855b
SANO Masatoshi <snmsts@gmail.com>
2016-07-02 12:20:58 +0900

    add uname info on filename.

--- scripts/roswell.sh
+++ scripts/roswell.sh
@@ -13,5 +13,5 @@ cp roswell/lisp/*.lisp roswell/lisp/*.ros archive/etc/roswell
 cp scripts/Makefile.roswell archive/Makefile
 rm -rf roswell
 mv archive roswell
-tar czf roswell-bin-$1.tgz roswell
-./scripts/upload.sh roswell-bin-$1.tgz roswell-bin-$1 $GITHUB_OWNER $GITHUB_REPO
+tar czf roswell-bin-$1-`uname`.tgz roswell
+./scripts/upload.sh roswell-bin-$1-`uname`.tgz roswell-bin-$1 $GITHUB_OWNER $GITHUB_REPO
