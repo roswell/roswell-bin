fdeb6fdc44a4512f7762e709a0596756b58d00f4
SANO Masatoshi <snmsts@gmail.com>
2016-07-03 18:52:34 +0900

    subtle

--- scripts/dispatch.sh
+++ scripts/dispatch.sh
@@ -3,7 +3,8 @@ TARGET=`grep TARGET target |awk '{print $2}'`
 VERSION=`grep VERSION target |awk '{print $2}'`
 
 ./scripts/clean.sh
-./scripts/$TARGET.sh $VERSION
-if [ x$1 = xupload ]; then
-  ./scripts/$TARGET-upload.sh $2 $VERSION;
+if [ x$1 = x ]; then
+    ./scripts/$TARGET.sh $VERSION
+elif [ x$1 = xupload ]; then
+    ./scripts/$TARGET-upload.sh $2 $VERSION;
 fi
